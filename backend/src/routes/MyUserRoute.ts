1cde51e3c2ba9fa586c25043601fd8ebd72c181d
joshua hassan <cutejosh2@gmail.com>
2024-11-03 00:19:55 +0100

    update user profile details endpoint, add validatoin middleware

--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -1,11 +1,12 @@
 import express from "express"
 import MyUserController from "../controllers/MyUserController"
-import { jwtCheck } from "../middleware/auth"
+import { jwtCheck, jwtParse } from "../middleware/auth"
+import { validateMyUserRequest } from "../middleware/validation"
 
 const router = express.Router()
 
 // /api/my/user
 router.post("/", jwtCheck, MyUserController.createCurrenUser)
-
+router.put("/", jwtCheck, jwtParse, validateMyUserRequest, MyUserController.updateCurrentUser)
 
 export default router
