37af072a5b0e6b899d4823aea3b4b653b49c081d
joshua hassan <cutejosh2@gmail.com>
2024-11-03 02:32:05 +0100

    add scripts to  backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,12 +21,11 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.use("/api/my/user", myUserRoute);
-
-app.get("/test", async (req: Request, res: Response) => {
-    res.json({ message: "hi no" })
+app.get("/health", async (req: Request, res: Response) => {
+    res.json({ message: "health ok!" })
 })
 
+app.use("/api/my/user", myUserRoute);
 
 app.listen(7000, () => {
     console.log("server started on localhost:7000")
